7d5fade652a624c33a3de4bfd98edc5b3dd68554
dr12lol <dramir2306@gmail.com>
2025-09-24 18:49:49 -0600

    First version testing

---
+++ script.js
@@ -0,0 +1,27 @@
+changeColours()
+changeSize()
+
+
+async function changeColours() {
+    while (true){
+        const hue = Math.random() * 360
+        const brightness = Math.random() * 100
+        const color = `hsl(${hue}, 80%, 50%)`
+        document.getElementById('titleb').style.color = color
+        await wait(500)
+    }
+}
+function wait(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+async function changeSize() {
+    while (true){
+        const size = Math.random() * 50
+
+        document.getElementById('titlec').style.fontSize = size+"px"
+        await wait(500)
+    }
+}
+function wait(ms){
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
